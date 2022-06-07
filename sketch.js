c82eab2123b9d648b72402c449aa32fad0fa53e8
ianzmn <90656535+ianzmn@users.noreply.github.com>
2022-06-07 15:06:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,11 +35,11 @@ function drawHand() {
   stroke(255);//second
   line(x,y,x - (160*cos(timeToAngle (second, 0))), y - (160*sin(timeToAngle (second,0))));
   
-  stroke(#C0C0C0);//minute
+  stroke(80);//minute
   strokeWeight(3);
   line(x,y,x-(120*cos(timeToAngle(minute, 0))), y-(120*sin(timeToAngle (minute,0))));
   
-  stroke(#d4af37) ;//hour
+  stroke(100) ;//hour
   strokeWeight(5) ;
   line(x,y,x - (90*cos(timeToAngle (hour, 1))),y- (90*sin(timeToAngle (hour,1))));
 } 
